6904d59945dbb3249cade4f650189eba9b65490c
dshinde <dnyaneshwarshinde33368@gmail.com>
2024-02-01 12:28:09 +0530

    minor fixes

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
     const img_src = "https://th.bing.com/th/id/R.9d32bec8058bd3595a63a08a8cc12ade?rik=9cCTin36GLU%2f5w&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_87237.png&ehk=hVpH%2bC7rwlA1j2KqxGpMs1sp9l0RgM0jjRJsJsvDoPc%3d&risl=&pid=ImgRaw&r=0";
     const navigate = useNavigate();
     useEffect(() => {
-        if (!sessionStorage.getItem('authTocken')) {
+        if (!sessionStorage.getItem('authTocken') || sessionStorage.getItem('authTocken')===null) {
             navigate('/login');
         }
         const s = io.connect("http://localhost:8000/home", {
