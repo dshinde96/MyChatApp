96ce1ea6577b46f370a21fd8b2e3d93733dfbe5e
dshinde <dnyaneshwarshinde33368@gmail.com>
2024-02-01 15:08:12 +0530

    minor style fixes

--- client/src/Components/ChatPage.js
+++ client/src/Components/ChatPage.js
@@ -8,6 +8,7 @@ const ChatCnt = () => {
     const [ChatMsg, setChatMsg] = useState([]);
     const [textMsg, setTextMsg] = useState();
     const [socket, setSocket] = useState();
+    const [msgType,setMsgType]=useState('text');
 
 
     const img_src = "https://th.bing.com/th/id/R.9d32bec8058bd3595a63a08a8cc12ade?rik=9cCTin36GLU%2f5w&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_87237.png&ehk=hVpH%2bC7rwlA1j2KqxGpMs1sp9l0RgM0jjRJsJsvDoPc%3d&risl=&pid=ImgRaw&r=0";
@@ -71,7 +72,11 @@ const ChatCnt = () => {
                         )}
                     </div>
                     <div className="newMsgCnt">
-                        <input type="text" value={textMsg} className="mx-2" placeholder="Enter your Message" onChange={(e) => setTextMsg(e.target.value)} />
+                    <select onChange={(e)=>setMsgType(e.target.value)}>
+                        <option value='text'>Text</option>
+                        <option value='file'>File</option>
+                    </select>
+                        <input type={`${msgType}`} value={textMsg} className="mx-2" placeholder="Enter your Message" onChange={(e) => setTextMsg(e.target.value)} />
                         <button className="btn btn-primary mx-2" type="button" onClick={SendMsg}>Send</button>
                     </div>
                 </div>
