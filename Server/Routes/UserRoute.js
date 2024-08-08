b6d1f4853084678fb342307bc70644917e449d32
dshinde <dnyaneshwarshinde33368@gmail.com>
2024-08-08 10:15:44 +0530

    Small refactoring to server side

--- Server/Routes/UserRoute.js
+++ Server/Routes/UserRoute.js
@@ -1,27 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const GoogleStrategy = require('passport-google-oauth2');
-const passport = require("passport");
 const { handleUserSignup, handleUserLogin, handleSignupByGoogle } = require('../Controllers/UserController')
 const { body } = require('express-validator');
-
-passport.use(new GoogleStrategy({
-    clientID: "88701807115-l14flim3lg8ptm2cp5p17ed184qm8dat.apps.googleusercontent.com",
-    clientSecret: "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",
-    callbackURL: "http://localhost:8000/user/auth/google/callback",
-}, (req, accessToken, refreshToken, profile, done) => {
-    return done(null, profile);
-}));
-
-passport.serializeUser((user, done) => {
-    // console.log(user);
-    done(null, user)
-})
-
-passport.deserializeUser((user, done) => {
-    console.log(user); 
-    done(null, user)
-})
 
 router.post('/signup',
     body('name', 'Name Cannot be empty').notEmpty(),
@@ -31,13 +11,4 @@ router.post('/signup',
 router.post('/login',
     body('email', "Enter a valid Email").isEmail(),
     body('password', 'password Cannot be empty').notEmpty(), handleUserLogin);
-
-router.get('/auth/google',
-    passport.authenticate('google', {
-        scope: ['email', 'profile']
-    }));
-router.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    handleSignupByGoogle
-);
 module.exports = router;
